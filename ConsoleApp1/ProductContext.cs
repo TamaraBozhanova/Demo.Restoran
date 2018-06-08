03e3c9e66d31d67f101007c54800050862cf102c
Tamara Bozhanova <tamara.bozhanova@gmail.com>
2018-06-08 14:20:12 +0300

    commit

---
+++ ConsoleApp1/ProductContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Demo.Restoran
+
+{
+    public class ProductContext: DbContext 
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+
+     public ProductContext():
+            base("ProductDB")
+        { }
+
+       
+
+
+    }
+}
